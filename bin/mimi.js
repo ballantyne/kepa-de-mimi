6c101d4ba1c2a473ad5e38c4f714162e9f73b466
Scott Ballantyne <ussballantyne@gmail.com>
2018-10-19 15:36:08 -0500

    new version

--- bin/mimi.js
+++ bin/mimi.js
@@ -11,14 +11,27 @@ var program = require('commander');
 program
   .version('0.1.0')
   .option('-i, --input <input>', 'Input File')
+  .option('-o, --output <output>', 'Output File')
   .option('-p, --password <password>', 'Password')
   .option('-v, --vector <vector>', 'Initialization Vector')
   .parse(process.argv);
 
+var output;
+
 if (program.args[0] == 'encrypt') {
-  Mimi.encrypt(program.password, program.input, program.vector);
+  if (program.output == undefined) {
+    output = [program.input, '.encrypted'].join('');
+  } else {
+    output = program.output;
+  }
+  Mimi.encrypt(program.password, program.input, output, program.vector);
 }
 
 if (program.args[0] == 'decrypt') {
-  Mimi.decrypt(program.password, program.input, program.vector);
+  if (program.output == undefined) {
+    output = program.input.replace('.encrypted', '');
+  } else {
+    output = program.output;
+  }
+  Mimi.decrypt(program.password, program.input, output, program.vector);
 }
